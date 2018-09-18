9d9162485cb02dc410236db60cabc24bffd2439c
Tushar <gupta.tus@husky.neu.edu>
2018-09-17 21:07:52 -0400

    Max Char done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+     const chars = {};
+
+     for (let char of str){
+         chars[char] =  chars[char] + 1 || 1;
+     }
+
+     let mostCommon = ''
+     let max = 0
+     for (let char of str){
+         if(max<chars[char]){
+            max = chars[char];
+            mostCommon = char
+         }         
+     }
+     return mostCommon
+}
 
 module.exports = maxChar;
