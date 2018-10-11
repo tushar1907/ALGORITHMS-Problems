c51af5735d4b32ec4c7e60a8cbcb007d8cac9157
Tushar <gupta.tus@husky.neu.edu>
2018-10-11 16:09:56 -0400

    pyramid Solution  1

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floor((2*n)-1)
+    for(let row = 0 ; row < n ; row = row + 1){
+        let level ='';
+        for(let col=0; col<((2*n)-1);col=col+1){            
+            if(midpoint - row <= col && midpoint + row >= col ){
+                level += '#'
+            }
+            else {
+                level += ' ';
+            }
+        }
+        console.log(level)
+    }
+    
+}
 
 module.exports = pyramid;
