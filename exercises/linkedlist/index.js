e2aada59c360510061a4c9ef8fc0fb3eee5f3f8a
Tushar <gupta.tus@husky.neu.edu>
2019-02-02 12:43:00 -0500

    Insertion in linked list done

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -6,11 +6,20 @@ class Node {
     constructor(data,next=null){
         this.data = data;
         this.next = next;
+    }  
+}
+
+class LinkedList {
+    constructor(){
+        this.head=null
+    }
+
+    insertFirst(data){
+        const node = new Node(data, this.head)  
+        this.head = node;        
     }
 
     
 }
-
-class LinkedList {}
 
 module.exports = { Node, LinkedList };
